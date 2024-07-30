e2ab424162142d19048d82ba203bbf8dce3be16d
Viorel Nanau <viorelnanau.redmusic@gmail.com>
2024-07-30 14:57:14 +0300

    add SURFSHARK prefix to vpn name

--- python-version/scripts/start_vpn.sh
+++ python-version/scripts/start_vpn.sh
@@ -7,4 +7,4 @@ echo "$ovpn_file" > /var/log/vpn.log
 sudo chmod 777 /var/log/vpn.log
 source /app/.env
 echo 'Starting openvpn ...'
-/app/python-version/scripts/start_vpn.exp $ovpn_file $VPN_USERNAME $VPN_PASSWORD
+/app/python-version/scripts/start_vpn.exp $ovpn_file $SURFSHARK_VPN_USERNAME $SURFSHARK_VPN_PASSWORD
